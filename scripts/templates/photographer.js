c4f06c7e7a2f7624bc8b5a28e70e776566a59047
vatr84 <vatr@hotmail.fr>
2024-07-23 00:49:00 +0200

    Page d'accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,32 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const link = document.createElement( 'a' );
+
+        const imgContainer = document.createElement( 'div' );
+        imgContainer.classList.add( 'photographer_card' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = `${city}, ${country}`;
+        const tag = document.createElement( 'p' );
+        tag.textContent = tagline;
+        const prix = document.createElement( 'p' );
+        prix.textContent = `${price}€/jour`;
+
+        article.appendChild(link);
+        imgContainer.appendChild(img);
+        link.appendChild(imgContainer);
+        link.appendChild(h2);
+        link.appendChild(h3);
+        link.appendChild(tag);
+        link.appendChild(prix);
         return (article);
     }
     return { name, picture, getUserCardDOM }
