ce1350663211af591aaf8a37d12048bee8b3ae5f
vatr84 <vatr@hotmail.fr>
2024-07-29 15:34:16 +0200

    attributs ARIA

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -5,31 +5,36 @@ function photographerTemplate(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const link = document.createElement( 'a' );
         link.href = `../../photographer.html?id=${id}`;
+        link.setAttribute('aria-label', `View profile of photographer ${name}`);
 
         const imgContainer = document.createElement( 'div' );
         imgContainer.classList.add( 'photographer_card' );
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         img.setAttribute("alt", `Portrait of ${name}, photographer from ${city}, ${country}`);
 
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
         const h3 = document.createElement( 'h3' );
         h3.textContent = `${city}, ${country}`;
+
         const tag = document.createElement( 'p' );
         tag.textContent = tagline;
+
         const prix = document.createElement( 'p' );
         prix.textContent = `${price}€/jour`;
 
         article.appendChild(link);
+
         imgContainer.appendChild(img);
-        link.appendChild(imgContainer);
-        link.appendChild(h2);
-        link.appendChild(h3);
-        link.appendChild(tag);
-        link.appendChild(prix);
+
+        link.append(imgContainer, h2, h3, tag, prix);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
